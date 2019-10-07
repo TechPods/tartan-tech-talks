f9d3738d55a5b031de1f38b033c5eb05f5316a00
teodora-techpods <teodora@techpods.co>
2019-10-07 20:19:35 +0300

    Homepage "Coming soon"

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import GlobalStyle from "../helpers/styles/globalStyles";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,31 +12,31 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
+      <GlobalStyle />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+          paddingTop: 0
+        }}>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          {/* © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.gatsbyjs.org">Gatsby</a> */}
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,33 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+import Logo from "../images/tartan-logo.svg";
+import styled from "styled-components";
+
+const HomeHero = styled.div`
+  text-align: center;
+  position: relative;
+  height: 100vh;
+  svg {
+    margin: 100px 0;
+    max-width: 100%;
+  }
+  h2 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    color: #a40105;
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
-    <h1>Coming soon</h1>
+    <HomeHero>
+      <Logo />
+      <h1>Tartan Tech Talks : Where tech leaders talk tech</h1>
+      <h2>Coming soon...</h2>
+    </HomeHero>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
