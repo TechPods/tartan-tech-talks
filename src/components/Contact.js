62d0591ac431e7bba02ef43f9578feb9da7c6c67
Veselin Stamenov <veselin@almond.org>
2019-10-31 13:08:16 +0200

    trying serverless

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,11 +29,20 @@ const ContactTitle = styled.h1`
   }
 `;
 
+const FormLabel = styled.label`
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+  margin-top: 10px;
+`;
+
 export default class ContactPage extends Component {
 
     state = {
-        name: '',
-        message: '',
+        firstName: '',
+        lastName: '',
+        company: '',
+        role: '',
         email: '',
         sent: false,
         buttonText: 'Send Message'
@@ -57,9 +66,11 @@ export default class ContactPage extends Component {
 
         let data = new FormData()
 
-        data.set('name', this.state.name)
+        data.set('firstName', this.state.firstName)
+        data.set('lastName', this.state.lastName)
+        data.set('company', this.state.company)
+        data.set('role', this.state.role)
         data.set('email', this.state.email)
-        data.set('message', this.state.message)
 
         axios({
             method: 'post',
@@ -81,16 +92,20 @@ export default class ContactPage extends Component {
                     Contact
                 </ContactTitle>
                 <form className="contact-form" onSubmit={(e) => this.formSubmit(e)}>
-                    <label className="message" htmlFor="message-input">Your Message</label>
-                    <textarea onChange={e => this.setState({message: e.target.value})} name="message"
-                              className="message-input" type="text" placeholder="Please write your message here"
-                              value={this.state.message} required/>
-
-                    <label className="message-name" htmlFor="message-name">Your Name</label>
-                    <input onChange={e => this.setState({name: e.target.value})} name="name" className="message-name"
-                           type="text" placeholder="Your Name" value={this.state.name}/>
-
-                    <label className="message-email" htmlFor="message-email">Your Email</label>
+                    <h3 style={{textAlign: 'center'}}>I'm interested in attending as:</h3>
+                    <FormLabel className="message-name" htmlFor="message-name">First Name</FormLabel>
+                    <input onChange={e => this.setState({firstName: e.target.value})} name="firstName" className="message-name"
+                           type="text" placeholder="First Name" value={this.state.firstName}/>
+                    <FormLabel className="message-name" htmlFor="message-name">Last Name</FormLabel>
+                    <input onChange={e => this.setState({lastName: e.target.value})} name="lastName" className="message-name"
+                           type="text" placeholder="Last Name" value={this.state.lastName}/>
+                    <FormLabel className="message-name" htmlFor="message-name">Company</FormLabel>
+                    <input onChange={e => this.setState({company: e.target.value})} name="company" className="message-name"
+                           type="text" placeholder="Company" value={this.state.company}/>
+                    <FormLabel className="message-name" htmlFor="message-name">Role</FormLabel>
+                    <input onChange={e => this.setState({role: e.target.value})} name="role" className="message-name"
+                           type="text" placeholder="Role" value={this.state.role}/>
+                    <FormLabel className="message-email" htmlFor="message-email">Email</FormLabel>
                     <input onChange={(e) => this.setState({email: e.target.value})} name="email"
                            className="message-email" type="email" placeholder="your@email.com" required
                            value={this.state.email}/>
