39a8828c666e5b22c07c6549a4bcf7bbe51d8613
Veselin Stamenov <veselin@almond.org>
2019-11-01 11:28:23 +0200

    form complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,13 @@ import axios from 'axios'
 import breakpoints from '../helpers/styles/breakpoints';
 
 const ContactContainer = styled.div`
+  margin-top: 30px;
+  height: calc(100vh - 200px);
   display: block;
+  padding: 30px;
   position: relative;
-  padding: 25px;
+  padding-left: 60px;
+  padding-left:
   @media ${breakpoints.mobileX} {
     width: calc(100% - 130px);
     margin: auto;
@@ -17,6 +21,7 @@ const ContactTitle = styled.h1`
     transform: rotate(90deg);
     transform-origin: left top 0;
     position: relative;
+    padding-left: 0px;
   }
 
   svg {
@@ -36,6 +41,59 @@ const FormLabel = styled.label`
   margin-top: 10px;
 `;
 
+const TextInput = styled.input`
+   border-radius: 8px;
+   width: 100%;
+   padding: 8px 12px;
+   border-width: 0;
+   box-shadow: 0 0 2px rgba(0,0,0,0.3);
+`;
+
+const FormComponent = styled.form`
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: left;
+`;
+
+const SubmitButton = styled.button`
+    width: 200px;
+    margin: 0 auto;
+    padding: 8px 12px;
+    background-color: #105327;
+    color: #fff;
+    border-radius: 8px;
+    margin-top: 20px;
+`;
+
+const ButtonContainer = styled.div`
+    width: 100%;
+    text-align: center;
+`;
+
+const ContactContent = styled.div`
+   border-left: 15px solid #105327;
+   text-align: center;
+`;
+
+const SwitchButton = styled.button`
+  outline: 0;
+  border: 1px;
+  text-align: center;
+padding: 8px 12px;
+  ${({ active }) => active && `
+    background: #105327;
+    color: #fff;
+  `}
+`;
+
+const SwitchContainer = styled.div`
+   border: 1px solid #105327;
+   display: inline-block;
+   margin: 0 auto;
+   margin-bottom: 20px;
+   margin-top: 10px;
+`;
+
 export default class ContactPage extends Component {
 
     state = {
@@ -45,17 +103,26 @@ export default class ContactPage extends Component {
         role: '',
         email: '',
         sent: false,
-        buttonText: 'Send Message'
+        buttonText: 'Send Message',
+        speaker: true
     }
 
     resetForm = () => {
         this.setState({
-            name: '',
-            message: '',
+            firstName: '',
+            lastName: '',
+            company: '',
+            role: '',
             email: '',
-            buttonText: 'Message Sent'
+            buttonText: 'Mesage sent',
+            speaker: true
         })
-    }
+    };
+
+    toggleClass = () => {
+        const currentState = this.state.speaker;
+        this.setState({ speaker: !currentState });
+    };
 
     formSubmit = (e) => {
         e.preventDefault()
@@ -71,10 +138,11 @@ export default class ContactPage extends Component {
         data.set('company', this.state.company)
         data.set('role', this.state.role)
         data.set('email', this.state.email)
+        data.set('type', this.state.speaker? 'Speaker': 'Attendee')
 
         axios({
             method: 'post',
-            url: 'https://tartanbackend.stameno.now.sh/api/v1',
+            url: 'http://localhost:4444/api/v1',
             data,
         })
             .then( res => {
@@ -91,29 +159,45 @@ export default class ContactPage extends Component {
                 <ContactTitle id="contact">
                     Contact
                 </ContactTitle>
-                <form className="contact-form" onSubmit={(e) => this.formSubmit(e)}>
+                <ContactContent>
                     <h3 style={{textAlign: 'center'}}>I'm interested in attending as:</h3>
-                    <FormLabel className="message-name" htmlFor="message-name">First Name</FormLabel>
-                    <input onChange={e => this.setState({firstName: e.target.value})} name="firstName" className="message-name"
-                           type="text" placeholder="First Name" value={this.state.firstName}/>
-                    <FormLabel className="message-name" htmlFor="message-name">Last Name</FormLabel>
-                    <input onChange={e => this.setState({lastName: e.target.value})} name="lastName" className="message-name"
-                           type="text" placeholder="Last Name" value={this.state.lastName}/>
-                    <FormLabel className="message-name" htmlFor="message-name">Company</FormLabel>
-                    <input onChange={e => this.setState({company: e.target.value})} name="company" className="message-name"
-                           type="text" placeholder="Company" value={this.state.company}/>
-                    <FormLabel className="message-name" htmlFor="message-name">Role</FormLabel>
-                    <input onChange={e => this.setState({role: e.target.value})} name="role" className="message-name"
-                           type="text" placeholder="Role" value={this.state.role}/>
-                    <FormLabel className="message-email" htmlFor="message-email">Email</FormLabel>
-                    <input onChange={(e) => this.setState({email: e.target.value})} name="email"
-                           className="message-email" type="email" placeholder="your@email.com" required
-                           value={this.state.email}/>
+                    <SwitchContainer>
+                        <SwitchButton
+                            active={this.state.speaker}
+                            onClick={() => this.setState({speaker: true})}
+                        >
+                            Speaker
+                        </SwitchButton>
+                        <SwitchButton
+                            active={!this.state.speaker}
+                            onClick={() => this.setState({speaker: false})}
+                        >
+                            Attendee
+                        </SwitchButton>
+                    </SwitchContainer>
+                    <FormComponent onSubmit={(e) => this.formSubmit(e)}>
+                        <FormLabel htmlFor="message-name">First Name</FormLabel>
+                        <TextInput onChange={e => this.setState({firstName: e.target.value})} name="firstName" className="message-name"
+                               type="text" placeholder="First Name" value={this.state.firstName}/>
+                        <FormLabel htmlFor="message-name">Last Name</FormLabel>
+                        <TextInput onChange={e => this.setState({lastName: e.target.value})} name="lastName" className="message-name"
+                               type="text" placeholder="Last Name" value={this.state.lastName}/>
+                        <FormLabel htmlFor="message-name">Company</FormLabel>
+                        <TextInput onChange={e => this.setState({company: e.target.value})} name="company" className="message-name"
+                               type="text" placeholder="Company" value={this.state.company}/>
+                        <FormLabel htmlFor="message-name">Role</FormLabel>
+                        <TextInput onChange={e => this.setState({role: e.target.value})} name="role" className="message-name"
+                               type="text" placeholder="Role" value={this.state.role}/>
+                        <FormLabel htmlFor="message-email">Email</FormLabel>
+                        <TextInput onChange={(e) => this.setState({email: e.target.value})} name="email"
+                               className="message-email" type="email" placeholder="your@email.com" required
+                               value={this.state.email}/>
 
-                    <div className="button--container">
-                        <button type="submit" className="button button-primary">{this.state.buttonText}</button>
-                    </div>
-                </form>
+                        <ButtonContainer>
+                            <SubmitButton type="submit" className="button button-primary">{this.state.buttonText}</SubmitButton>
+                        </ButtonContainer>
+                    </FormComponent>
+                </ContactContent>
             </ContactContainer>
         )
     }
